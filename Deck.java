df09991b3e764c35e4a3dbff901661bd0beb13f7
shuhan1997 <32233195+shuhan1997@users.noreply.github.com>
2017-11-10 11:52:13 +0800

    20171110

---
+++ Deck.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Deck {
+
+		private ArrayList<Card> cards;
+		private ArrayList<Card> usedCard;
+		public int nUsed=0;
+		public Deck(int nDeck){
+			cards=new ArrayList<Card>();
+			usedCard=new ArrayList<Card>();
+			int deck, flower, number;
+			for(deck = 0; deck < nDeck; deck++) 
+			{
+				for(flower = 1; flower < 5; flower++) 
+				{
+					for(number = 1; number < 14; number++) 
+					{
+						Card card = new Card(flower, number);
+						cards.add(card);
+					}
+				}
+					
+			}
+			shuffle();			
+		}	
+		public void shuffle() 
+		{
+			Random rnd = new Random();
+			while (usedCard.size()>0) 
+			{
+				Card backCard;
+				backCard = usedCard.get(0);
+				usedCard.remove(0);
+				cards.add(backCard);
+			}
+			for(int a = 0; a < cards.size();a++) 
+			{
+				Card cardPlace;
+				cardPlace = cards.get(a);
+				int j = rnd.nextInt(cards.size());
+				cards.set(a, cards.get(j));
+				cards.set(j, cardPlace);
+			}
+			nUsed = 0;
+		}
+		public Card getOneCard() 
+		{
+			if (cards.size()>0) 
+			{
+				Card getCard;
+				getCard = cards.get(0);
+				usedCard.add(getCard);
+				cards.remove(0);
+				nUsed++;
+				return getCard;
+								
+			}
+			else
+			{
+				shuffle();
+				getOneCard();
+				return null;
+			}
+		}
+		public void printDeck(){
+			int deck;
+			for(deck = 0; deck < cards.size(); deck++) 
+			{
+				Card poker = cards.get(deck);
+				poker.printCard();
+			}
+		}
+		public ArrayList<Card> getAllCards(){
+			return cards;
+		}
+	}
